7e41d453c93d9a734e515ea5348795e60d5ca1b4
Rachyita Singh <veereshsingh0412@gmail.com>
2025-04-28 15:04:01 +0530

    submit

---
+++ lec7.js
@@ -0,0 +1,41 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const fs=require('fs');
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/',function(req,res){
+    
+
+
+ 
+     fs.readdir('./files',function(err,files){
+    //     console.log(files);// this is how read dir works
+    res.render('index',{files:files});    // render se hm koi b file dikha skte hai local host pe
+     })
+
+
+
+    // res.send("Welcome");   //  isse ye welcome hi dikhega localhost pe
+
+})
+app.get('/file/:filename',function(req,res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err,filedata){
+        res.render('show',{filename:req.params.filename, filedata: filedata});
+        
+    });
+    
+});
+
+app.post('/create',function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
+        res.redirect("/")
+    });
+    
+})
+
+app.listen(3000);
